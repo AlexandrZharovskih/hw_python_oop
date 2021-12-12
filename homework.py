8b385e7c0bc23ac09ab26be0cd5d49483d78c8ed
AlexanderZharovskih <90411366+AlexanderZharovskih@users.noreply.github.com>
2021-12-12 20:01:12 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -64,7 +64,7 @@ class Running(Training):
 
     coeff_calorie_1 = 18
     coeff_calorie_2 = 20
-    training_time_in_minutes = 60  # minutes
+    training_time_in_mints = 60  # minutes
 
     def __init__(self, action: int, duration: float, weight: float) -> None:
         super().__init__(action, duration, weight)
@@ -72,7 +72,7 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         return ((self.coeff_calorie_1 * self.get_mean_speed()
                 - self.coeff_calorie_2) * self.weight / self.M_IN_KM
-                * (self.duration * self.training_time_in_minutes))
+                * (self.duration * self.training_time_in_mints))
 
 
 class SportsWalking(Training):
@@ -80,7 +80,7 @@ class SportsWalking(Training):
 
     coeff_calorie_3 = 0.035
     coeff_calorie_4 = 0.029
-    training_time_in_minutes = 60  # minutes
+    training_time_in_mints = 60  # minutes
 
     def __init__(self,
                  action: int,
@@ -94,8 +94,7 @@ class SportsWalking(Training):
         return (self.coeff_calorie_3 * self.weight
                 + (self.get_mean_speed()**2 // self.height)
                 * self.coeff_calorie_4
-                * self.weight) * (self.duration
-                * self.training_time_in_minutes)
+                * self.weight) * (self.duration * self.training_time_in_mints)
 
 
 @dataclass
